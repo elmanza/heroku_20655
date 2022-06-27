fa1720229e53d207f93413ad23e2e51386f7cd0b
Andres Manzano <andres.manzano@exclusivecontent.me>
2022-06-27 17:53:15 -0500

    Se creo un HTML

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ let express = require("express");
 let app = express();
 const PORT = 8080;
 
-app.get("/", (req,res)=>{res.send("Todo ok!")});
+
+app.use(express.static("public"));
+
+// app.get("/", (req,res)=>{res.send("Todo ok!")});
 
 
 app.listen(PORT, ()=>{console.log(`http://localhost:${PORT}`)});
